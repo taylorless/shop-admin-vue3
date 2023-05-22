60a44c5b740e1ad552170f89c562b8efa3086e06
lishanghang <630664429@qq.com>
2023-05-22 17:01:36 +0800

    初始化

---
+++ shop-admin-vue3/src/router/index.js
@@ -0,0 +1,10 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+
+const routes = [];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  router: router,
+});
+
+export default router;
